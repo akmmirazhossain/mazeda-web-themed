56173aede91242ee1e3d5753d704e53414a04817
AKM Miraz Hossain <akmmirazhossain@gmail.com>
2024-02-01 08:02:04 +0600

    Add our clients section, logo repositioned

--- pages/components/ClientsSection.js
+++ pages/components/ClientsSection.js
@@ -8,39 +8,49 @@ const ClientsSection = () => {
           Successful Partners
         </h1>
       </div>
-      <div className="flex gap-4 justify-between py-4">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-5 gap-4 py-4">
         {" "}
         {/* Adjust gap as needed */}
-        <Image
-          src="/images/logos/dummy-logo.png"
-          alt="Client 1 Logo"
-          width={200}
-          height={50}
-        />
-        <Image
-          src="/images/logos/dummy-logo.png"
-          alt="Client 1 Logo"
-          width={200}
-          height={50}
-        />
-        <Image
-          src="/images/logos/dummy-logo.png"
-          alt="Client 3 Logo"
-          width={200}
-          height={50}
-        />
-        <Image
-          src="/images/logos/dummy-logo.png"
-          alt="Client 4 Logo"
-          width={200}
-          height={50}
-        />
-        <Image
-          src="/images/logos/dummy-logo.png"
-          alt="Client 5 Logo"
-          width={200}
-          height={50}
-        />
+        <div className="flex items-center justify-center h-full">
+          <Image
+            src="/images/logos/dummy-logo.png"
+            alt="Client 1 Logo"
+            width={200}
+            height={50}
+          />
+        </div>
+        <div className="flex items-center justify-center h-full">
+          <Image
+            src="/images/logos/dummy-logo.png"
+            alt="Client 1 Logo"
+            width={200}
+            height={50}
+          />
+        </div>
+        <div className="flex items-center justify-center h-full">
+          <Image
+            src="/images/logos/dummy-logo.png"
+            alt="Client 1 Logo"
+            width={200}
+            height={50}
+          />
+        </div>
+        <div className="flex items-center justify-center h-full">
+          <Image
+            src="/images/logos/dummy-logo.png"
+            alt="Client 1 Logo"
+            width={200}
+            height={50}
+          />
+        </div>
+        <div className="flex items-center justify-center h-full">
+          <Image
+            src="/images/logos/dummy-logo.png"
+            alt="Client 1 Logo"
+            width={200}
+            height={50}
+          />
+        </div>
       </div>
     </div>
   );
